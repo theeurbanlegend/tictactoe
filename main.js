1ef539779613d46fb6205df2e010bd19477d1f6b
theeurbanlegend <mbuto152@gmail.com>
2023-06-08 01:43:13 +0300

    Cool update

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-let init=0;
+/* let init=0;
 
 function loader() {
-  init = setTimeout(showPage, 6500);
+  init = setTimeout(showPage, 6200);
 }
 
 function showPage() {
   document.getElementById("preloader").style.display = "none";
 
-}
+} */
